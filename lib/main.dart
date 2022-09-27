d60fd0400de7512363fc566049afae00e8c506b1
berhanudagnew <berhanudagnew.github@gmail.com>
2022-09-28 02:58:21 +0300

    adding separate line

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,11 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
